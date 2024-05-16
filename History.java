a53922d1dfb3a2b0699bac8b68ed5837672c1764
lnolland <127619531+lnolland@users.noreply.github.com>
2024-05-16 17:04:59 +0200

    Update History.java

--- History.java
+++ History.java
@@ -4,39 +4,43 @@ public class History {
 	private int length;
 	private List<Book> books;
 	private List<Date> dates;
-	private List<Integer> lates;
+	private List<Integer> borrowDurations;
+	private List<Integer> howLates;
 	
 	public History()
 	{
 		this.length = 0;
 		this.books = new ArrayList<Book>();
 		this.dates = new ArrayList<Date>();
-		this.lates = new ArrayList<Integer>();
+		this.borrowDurations = new ArrayList<Integer>();
+		this.howLates = new ArrayList<Integer>();
 	}
 	
-	public void add(Book book, Date date, int late)
+	public void add(Book book, Date date, int duration, int late)
 	{
 		this.length++;
 		this.books.add(book);
 		this.dates.add(date);
-		this.lates.add(late);
+		this.borrowDurations.add(duration);
+		this.howLates.add(late);
 	}
 	
 	public List<Book> getBooks()
 	{
 		return this.books;
 	}
-	
 	public List<Date> getDates()
 	{
 		return this.dates;
 	}
-	
-	public List<Integer> getLates()
+	public List<Integer> getHowLates()
 	{
-		return this.lates;
+		return this.howLates;
 	}
-	
+	public List<Integer> getBorrowDurations()
+	{
+		return this.borrowDurations;
+	}
 	public int getLength()
 	{
 		return this.length;
