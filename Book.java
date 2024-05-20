edae1da8ef23f5266692831e4d9dc8519524b478
lnolland <127619531+lnolland@users.noreply.github.com>
2024-05-20 16:37:23 +0200

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,6 +1,4 @@
 import java.util.*;
-import java.io.*;
-import java.net.*;
 
 public class Book {
 	private List<ExemplaryBook> exemplaries;
@@ -64,36 +62,5 @@ public class Book {
 			
 		}
 		return sum;
-	}
-	
-	public boolean containsWord(String word)
-	{
-		//////// API 
-		/*
-		String apiUrl = "http://catalogue.bnf.fr/api/SRU";
-        String queryString = String.format("bib.anywhere %s \"%s\"",
-                relation,
-                URLEncoder.encode(query, "UTF-8"));
-        String urlString = apiUrl + "?version=1.2&operation=searchRetrieve&query=" + URLEncoder.encode(queryString, "UTF-8");
-
-        // Faire la requête HTTP
-        URL url = new URL(urlString);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        
-		 */
-		
-		// toLowerCase is used so 'A' match with 'a' (contains is case sensitive)
-		word = word.toLowerCase();
-		
-		// we verify if the word is contained in either
-		if(this.title.toLowerCase().contains(word) || this.author.toLowerCase().contains(word) || // the title or the genre
-		this.genre.toLowerCase().contains(word) || // the author's name
-		String.valueOf(this.isbn).toLowerCase().contains(word)) // the ISBN
-		{
-			return true;
-		}
-		
-		return false;
 	}
 }
