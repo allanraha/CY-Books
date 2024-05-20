eefb94da9627f2667863abfd1418e86f681d6d8e
lnolland <127619531+lnolland@users.noreply.github.com>
2024-05-20 16:36:42 +0200

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,4 +1,16 @@
+import java.io.*;
 import java.util.*;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.xml.sax.InputSource;
+import javax.xml.parsers.*;
+import org.w3c.dom.Element;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class Library {
 
@@ -29,28 +41,129 @@ public class Library {
         return this.users.get(id);
     }
     
-    //
-    public List<Book> searchBooks(String keyWord, String ...keyWords)
-    {
-    	List<Book> results = new ArrayList<Book>();
-    	
-    	for (Book book : this.books.values()) // for each book
-    	{
-    		if(book.containsWord(keyWord)) // if the book contains the keyWord parameter
-    		{
-    			results.add(book);	// add it to the list of results
-    		}
-    		else // verify if the book contains one of the other keywords so
-    		{
-    			for(String word : keyWords) // for each other keyword
-    			{
-    				if(book.containsWord(word)) // if the book contains it
-    	    			results.add(book); // add it to the list 
-    			}
-	    	}
-    	}
-        return results; // return the list
+
+    public static List<Map<String, String>> searchBooks(String queryString)
+            throws IOException, InterruptedException, ParserConfigurationException, SAXException {
+        // Construire l'URL de recherche
+        String apiUrl = "http://catalogue.bnf.fr/api/SRU";
+        String urlString = apiUrl + "?version=1.2&operation=searchRetrieve&query=" + URLEncoder.encode(queryString, "UTF-8") + "&recordSchema=dublincore";
+       
+	// Create a HTTP client
+        HttpClient client = HttpClient.newHttpClient();
+
+        // Create a HTTP request
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlString))
+                .build();
+
+        // Send the request and get the answer
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Parse the xml file received form the output
+        List<Map<String, String>> booksInfo = parseXMLResponse(response.body());
+
+       return booksInfo;
     }
+
+    public static List<Map<String, String>> parseXMLResponse(String xmlString) {
+        List<Map<String, String>> booksInfo = new ArrayList<>();
+
+        try {
+            // Convert the XML string to an InputStream using UTF-8 encoding
+            InputStream is = new ByteArrayInputStream(xmlString.getBytes("UTF-8"));
+
+            // Create a DocumentBuilderFactory
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+            // Create a DocumentBuilder
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            // Parse the InputStream and build the Document object
+            Document document = builder.parse(new InputSource(is));
+
+            // Normalize the XML Structure
+            document.getDocumentElement().normalize();
+
+            // Get all records
+            NodeList records = document.getElementsByTagName("srw:record");
+
+            for (int i = 0; i < records.getLength(); i++) 
+            {
+                Map<String, String> bookInfo = new HashMap<>();
+                Node record = records.item(i);
+
+                // Get all elements with the tag "dc:creator"
+                NodeList creators = ((Element) record).getElementsByTagName("dc:creator");
+                for (int j = 0; j < creators.getLength(); j++) {
+                    bookInfo.put("Author" + (j > 0 ? (" " + (j + 1)) : ""), creators.item(j).getTextContent());
+                }
+
+                // Get all elements with the tag "dc:title"
+                NodeList titles = ((Element) record).getElementsByTagName("dc:title");
+                if(titles.item(0) != null)
+                	bookInfo.put("Title", titles.item(0).getTextContent());
+                
+
+                // Get all elements with the tag "dc:type"
+                NodeList types = ((Element) record).getElementsByTagName("dc:type");
+                if(types.item(0) != null)
+                	bookInfo.put("Document Type" , types.item(0).getTextContent());
+                
+
+                // Get all elements with the tag "dc:contributor"
+                NodeList contributors = ((Element) record).getElementsByTagName("dc:contributor");
+                for (int j = 0; j < contributors.getLength(); j++) {
+                    bookInfo.put("Contributor" + (j > 0 ? (" " + (j + 1)) : ""), contributors.item(j).getTextContent());
+                }
+
+                // Get other informations of the publishers
+                NodeList publishers = ((Element) record).getElementsByTagName("dc:publisher");
+                String publiString = "";
+                for (int j = 0; j < publishers.getLength(); j++) {
+                	publiString += publishers.item(j).getTextContent() + "  ";
+                }
+                bookInfo.put("Publisher", publiString);
+                
+                // Get the date
+                NodeList dates = ((Element) record).getElementsByTagName("dc:date");
+                if(dates.item(0) != null)
+                	bookInfo.put("Date", dates.item(0).getTextContent());
+                
+                // Get the ISBN or the link to the notice
+                NodeList identifiers = ((Element) record).getElementsByTagName("dc:identifier");
+                for (int j = 0; j < identifiers.getLength(); j++) {
+                    Node identifier = identifiers.item(j);
+                    if (identifier.getTextContent().contains("ISBN")) {
+                        bookInfo.put("ISBN", identifier.getTextContent());
+                    } else if (identifier.getTextContent().contains("http")) {
+                        bookInfo.put("Identifier", identifier.getTextContent());
+                    }
+                }
+
+                // Get the descriptions
+                NodeList descriptions = ((Element) record).getElementsByTagName("dc:description");
+                String descriptString = "";
+                for (int j = 0; j < descriptions.getLength(); j++) {
+                	descriptString += descriptions.item(j).getTextContent() + " ";
+                }
+                bookInfo.put("Description", descriptString);
+                
+                // Get the format
+                NodeList formats = ((Element) record).getElementsByTagName("dc:format");
+                if(formats.item(0) != null)
+                	bookInfo.put("Format", formats.item(0).getTextContent());
+                
+
+                booksInfo.add(bookInfo);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return booksInfo;
+    }
+
     
     public List<Book> mostBorrowed()
     {
@@ -59,7 +172,6 @@ public class Library {
     	int i = 0;
     	Book book;
     			
-    	
     	do{
     		book = null;
     		
