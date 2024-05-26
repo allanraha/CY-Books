d41360af1ac8e67de6be6bf6bbe3e50a09e75534
Allan Rahajarizafy <104799181+allanraha@users.noreply.github.com>
2024-05-26 23:07:38 +0200

    Update Book.java

--- src/main/java/com/example/cybooks/models/Book.java
+++ src/main/java/com/example/cybooks/models/Book.java
@@ -1,38 +1,71 @@
 package com.example.cybooks.models;
-
+/**
+ * The Book class represents a book in the CyBooks application.
+ * It contains details about the book such as title, author, and publisher.
+ */
 public class Book {
     private String title;
     private String author;
     private String publisher;
 
-
+/**
+     * Constructs a new Book with the specified title, author, and publisher.
+     *
+     * @param title     the title of the book
+     * @param author    the author of the book
+     * @param publisher the publisher of the book
+     */
     public Book(String title, String author, String publisher) {
         this.title = title;
         this.author = author;
         this.publisher = publisher;
 
     }
-
+    /**
+     * Gets the title of the book.
+     *
+     * @return the title of the book
+     */
     public String getTitle() {
         return title;
     }
-
+    /**
+     * Sets the title of the book.
+     *
+     * @param title the new title of the book
+     */
     public void setTitle(String title) {
         this.title = title;
     }
-
+    /**
+     * Gets the author of the book.
+     *
+     * @return the author of the book
+     */
     public String getAuthor() {
         return author;
     }
-
+    /**
+     * Sets the author of the book.
+     *
+     * @param author the new author of the book
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
-
+    /**
+     * Gets the publisher of the book.
+     *
+     * @return the publisher of the book
+     */
     public String getPublisher() {
         return publisher;
     }
-
+    /**
+     * Sets the publisher of the book.
+     *
+     * @param publisher the new publisher of the book
+     */
     public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
