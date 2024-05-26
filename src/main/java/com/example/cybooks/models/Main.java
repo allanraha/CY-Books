0ca75131946933fa2b8db588ea4d92f291c29851
lnolland <127619531+lnolland@users.noreply.github.com>
2024-05-26 22:36:23 +0200

    Update Main.java

--- src/main/java/com/example/cybooks/models/Main.java
+++ src/main/java/com/example/cybooks/models/Main.java
@@ -1,25 +1,27 @@
 package com.example.cybooks.models;
 
-import com.example.cybooks.models.Library;
-
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The Main class is the entry point for the CyBooks application.
+ * It performs a book search using the Library class and displays the search results.
+ */
 public class Main {
     public static void main(String[] args) {
         try {
-            // Recherche de livres avec un terme de recherche spécifique
-            String queryString = "bib.title any Jack   "; // Modifiez ce terme selon vos besoins
+            // Search for books with a specific search term
+            String queryString = "bib.title any Jack"; // Modify this term as needed
             List<Map<String, String>> searchResults = Library.searchBooks(queryString);
 
-            // Affichage des résultats de la recherche
-            System.out.println("Résultats de la recherche pour : " + queryString);
+            // Display the search results
+            System.out.println("Search results for: " + queryString);
             for (Map<String, String> bookInfo : searchResults) {
-                System.out.println("Titre : " + bookInfo.get("Title"));
-                System.out.println("Auteur : " + bookInfo.get("Author"));
-                System.out.println("Type de document : " + bookInfo.get("Document Type"));
-                System.out.println("Éditeur : " + bookInfo.get("Publisher"));
-                System.out.println("Date : " + bookInfo.get("Date"));
+                System.out.println("Title: " + bookInfo.get("Title"));
+                System.out.println("Author: " + bookInfo.get("Author"));
+                System.out.println("Document Type: " + bookInfo.get("Document Type"));
+                System.out.println("Publisher: " + bookInfo.get("Publisher"));
+                System.out.println("Date: " + bookInfo.get("Date"));
                 System.out.println("--------------------------------------------");
             }
         } catch (Exception e) {
